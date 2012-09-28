de6e9b2b9a30ea616f10a13f6bbd8e8bf0dd8fb7
Faiz Visram <faiz.visram@gmail.com>
2012-09-27 20:37:24 -0400

    Add get_recent_posts API

--- src/com/faizvisram/WordPressBlog/WordPress/WordPress.java
+++ src/com/faizvisram/WordPressBlog/WordPress/WordPress.java
@@ -14,11 +14,16 @@ import android.content.Context;
  */
 public class WordPress {
 	public static final String API_GET_CATEGORY_INDEX = "get_category_index";
-	
+	public static final String API_GET_RECENT_POSTS = "get_recent_posts";
+
 	public static final String KEY_STATUS = "status";
+	public static final String KEY_ID = "id";
 	public static final String KEY_CATEGORIES = "categories";
 	public static final String KEY_TITLE = "title";
-	
+	public static final String KEY_PARENT = "parent";
+	public static final String KEY_POSTS = "posts";
+
+	public static final String CATEGORY_ID_ALL = "-1";
 	public static final String STATUS_OK = "ok";
 	
 	/**
@@ -43,6 +48,24 @@ public class WordPress {
 						
 					}
 					
+				}
+			}
+		});
+	}
+	
+	public static void getRecentPosts(Context context, final OnReturnListener onReturnListener) {
+		ConnectionHandler.execute(context, API_GET_RECENT_POSTS, null, new OnReturnListener() {
+			
+			@Override
+			public void onReturn(ArrayList<Map<String, String>> result) {
+				if (result != null && result.size() > 0) {
+					Map<String, String> item = result.get(0);
+					if (STATUS_OK.equals(item.get(KEY_STATUS))) {
+						String categoriesArray =  item.get(KEY_POSTS);
+						
+						ArrayList<Map<String, String>> categories = ConnectionHandler.parseJsonArray(categoriesArray);
+						onReturnListener.onReturn(categories);	
+					}
 				}
 			}
 		});
