4239bd4aacce3d24b7f6c79e328ab79f454d278b
Faiz Visram <faiz.visram@gmail.com>
2012-10-11 20:39:44 -0400

    Add 'get_category_posts' API

--- src/com/faizvisram/WordPressBlog/WordPress/WordPress.java
+++ src/com/faizvisram/WordPressBlog/WordPress/WordPress.java
@@ -4,7 +4,11 @@
 package com.faizvisram.WordPressBlog.WordPress;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.content.Context;
 
@@ -15,7 +19,8 @@ import android.content.Context;
 public class WordPress {
 	public static final String API_GET_CATEGORY_INDEX = "get_category_index";
 	public static final String API_GET_RECENT_POSTS = "get_recent_posts";
-
+	public static final String API_GET_CATEGORY_POSTS = "get_category_posts";
+	
 	public static final String KEY_STATUS = "status";
 	public static final String KEY_ID = "id";
 	public static final String KEY_CATEGORIES = "categories";
@@ -61,9 +66,32 @@ public class WordPress {
 				if (result != null && result.size() > 0) {
 					Map<String, String> item = result.get(0);
 					if (STATUS_OK.equals(item.get(KEY_STATUS))) {
-						String categoriesArray =  item.get(KEY_POSTS);
+						String posts = item.get(KEY_POSTS);
 						
-						ArrayList<Map<String, String>> categories = JsonParser.parseArray(categoriesArray);
+						ArrayList<Map<String, String>> categories = JsonParser.parseArray(posts);
+						onReturnListener.onReturn(categories);	
+					}
+				}
+			}
+		});
+	}
+	
+	public static void getCategoryPosts(Context context, String categoryId, final OnReturnListener onReturnListener) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		
+		// add parameters for HTTP POST
+		params.add(new BasicNameValuePair(KEY_ID, categoryId));
+		
+		ConnectionHandler.execute(context, API_GET_CATEGORY_POSTS, params, new OnReturnListener() {
+			
+			@Override
+			public void onReturn(ArrayList<Map<String, String>> result) {
+				if (result != null && result.size() > 0) {
+					Map<String, String> item = result.get(0);
+					if (STATUS_OK.equals(item.get(KEY_STATUS))) {
+						String posts = item.get(KEY_POSTS);
+						
+						ArrayList<Map<String, String>> categories = JsonParser.parseArray(posts);
 						onReturnListener.onReturn(categories);	
 					}
 				}
